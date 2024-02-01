c12c364a9b552509d35039b3fb8068315c27ed47
Zé Marcos <contato.marcosrf@gmail.com>
2024-02-01 13:56:08 -0300

    wip: finish testing for MarkDownParse class

--- tests/Feature/Support/MarkDownParseTest.php
+++ tests/Feature/Support/MarkDownParseTest.php
@@ -19,3 +19,33 @@ it('should be able parse markdown', function () {
     expect($post)->toHaveProperty('image');
     expect($post)->toHaveProperty('created_at');
 });
+
+it('should be able get all posts', function () {
+
+    $fileSystem = new Filesystem();
+
+    $markdown = new \App\Support\MarkDownParse($fileSystem);
+    $posts = $markdown->all();
+
+    expect($posts)->toBeInstanceOf(\Illuminate\Support\Collection::class);
+    expect($posts->first())->toHaveProperty('title');
+    expect($posts->first())->toHaveProperty('body');
+    expect($posts->first())->toHaveProperty('slug');
+    expect($posts->first())->toHaveProperty('image');
+    expect($posts->first())->toHaveProperty('created_at');
+});
+
+it('should be able paginate posts', function () {
+
+    $fileSystem = new Filesystem();
+
+    $markdown = new \App\Support\MarkDownParse($fileSystem);
+    $posts = $markdown->simplePaginate(9);
+
+    expect($posts)->toBeInstanceOf(\Illuminate\Pagination\LengthAwarePaginator::class);
+    expect($posts->first())->toHaveProperty('title');
+    expect($posts->first())->toHaveProperty('body');
+    expect($posts->first())->toHaveProperty('slug');
+    expect($posts->first())->toHaveProperty('image');
+    expect($posts->first())->toHaveProperty('created_at');
+});
