1a56eb44227233536c0c35c047e601298f6dc013
Zé Marcos <contato.marcosrf@gmail.com>
2024-02-01 17:45:10 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,25 @@
 <?php
 
 use App\Http\Controllers\{PostController, TestController};
+use App\Support\MarkDownParse;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('posts.index');
 });
 
-Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('posts/{slug}', [PostController::class, 'show'])->name('posts.show');
+Route::get('posts/{slug}', function (MarkDownParse $markdown, $slug) {
+    return view('posts.show', [
+        'post' => $markdown->find($slug . '.md')
+    ]);
+})->name('posts.show');
+
+Route::get('posts', function (MarkDownParse $markdown){
+    return view('posts.index', [
+        'posts' => $markdown->simplePaginate(9)
+    ]);
+})->name('posts.index');
+
 Route::get('contact', function () {
     return view('contact');
 })->name('contact');
